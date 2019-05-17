7748fe96667f75d192536a418fc4ff2e11e53972
Chris PMaag <chrispmaag@gmail.com>
2019-05-16 23:25:41 -0700

    Add DROP TABLE commands

---
+++ project_3_data_warehouse_redshift/sql_queries.py
@@ -0,0 +1,71 @@
+import configparser
+
+
+# CONFIG
+config = configparser.ConfigParser()
+config.read('dwh.cfg')
+
+# DROP TABLES
+
+staging_events_table_drop = "drop table if exists staging_events"
+staging_songs_table_drop = "drop table if exists staging_songs"
+songplay_table_drop = "drop table if exists songplays"
+user_table_drop = "drop table if exists users"
+song_table_drop = "drop table if exists songs"
+artist_table_drop = "drop table if exists artists"
+time_table_drop = "drop table if exists time"
+
+# CREATE TABLES
+
+staging_events_table_create= ("""create table staging_events
+""")
+
+staging_songs_table_create = ("""create table staging_songs
+""")
+
+songplay_table_create = ("""create table sonplays
+""")
+
+user_table_create = ("""create table users
+""")
+
+song_table_create = ("""create table songs
+""")
+
+artist_table_create = ("""create table artists
+""")
+
+time_table_create = ("""create table time
+""")
+
+# STAGING TABLES
+
+staging_events_copy = ("""
+""").format()
+
+staging_songs_copy = ("""
+""").format()
+
+# FINAL TABLES
+
+songplay_table_insert = ("""
+""")
+
+user_table_insert = ("""
+""")
+
+song_table_insert = ("""
+""")
+
+artist_table_insert = ("""
+""")
+
+time_table_insert = ("""
+""")
+
+# QUERY LISTS
+
+create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+copy_table_queries = [staging_events_copy, staging_songs_copy]
+insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
